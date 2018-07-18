0c16a428d47aff1a89bfde45b454c9b621a7625a
Monica Dinculescu <noms@google.com>
2018-07-18 15:59:10 -0700

    run npm format

--- element-styles/paper-item-styles.js
+++ element-styles/paper-item-styles.js
@@ -1,11 +1,42 @@
+/**
+@license
+Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+This code may only be used under the BSD style license found at
+http://polymer.github.io/LICENSE.txt The complete set of authors may be found at
+http://polymer.github.io/AUTHORS.txt The complete set of contributors may be
+found at http://polymer.github.io/CONTRIBUTORS.txt Code distributed by Google as
+part of the polymer project is also subject to an additional IP rights grant
+found at http://polymer.github.io/PATENTS.txt
+*/
+/**
+Material design:
+[Lists](https://www.google.com/design/spec/components/lists.html)
+
+Shared styles for a native `button` to be used as an item in a `paper-listbox`
+element:
+
+    <custom-style>
+      <style is="custom-style" include="paper-item-styles"></style>
+    </custom-style>
+
+    <paper-listbox>
+      <button class="paper-item" role="option">Inbox</button>
+      <button class="paper-item" role="option">Starred</button>
+      <button class="paper-item" role="option">Sent mail</button>
+    </paper-listbox>
+
+@group Paper Elements
+@demo demo/index.html
+*/
+
 import '@polymer/polymer/polymer-legacy.js';
 import '../color.js';
 import '../default-theme.js';
 import '../typography.js';
-const $_documentContainer = document.createElement('template');
-$_documentContainer.setAttribute('style', 'display: none;');
 
-$_documentContainer.innerHTML = `<dom-module id="paper-item-styles">
+import {html} from '@polymer/polymer/lib/utils/html-tag.js';
+const template = html`
+<dom-module id="paper-item-styles">
   <template>
     <style>
       :host, html {
@@ -62,39 +93,5 @@ $_documentContainer.innerHTML = `<dom-module id="paper-item-styles">
     </style>
   </template>
 </dom-module>`;
-
-document.head.appendChild($_documentContainer.content);
-
-/**
-@license
-Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-/**
-Material design: [Lists](https://www.google.com/design/spec/components/lists.html)
-
-Shared styles for a native `button` to be used as an item in a `paper-listbox` element:
-
-    <custom-style>
-      <style is="custom-style" include="paper-item-styles"></style>
-    </custom-style>
-
-    <paper-listbox>
-      <button class="paper-item" role="option">Inbox</button>
-      <button class="paper-item" role="option">Starred</button>
-      <button class="paper-item" role="option">Sent mail</button>
-    </paper-listbox>
-
-@group Paper Elements
-@demo demo/index.html
-*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
-;
+template.setAttribute('style', 'display: none;');
+document.head.appendChild(template.content);
